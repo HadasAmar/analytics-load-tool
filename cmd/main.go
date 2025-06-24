71423f600d4884971f94e4a9d9b8e1a6561b819e
Hadas <h0527622740@gmail.com>
2025-06-25 00:19:58 +0300

    chore: add go.mod and go.sum files

--- cmd/main.go
+++ cmd/main.go
@@ -13,6 +13,7 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+	"github.com/hashicorp/consul/api"
 )
 
 func main() {
@@ -98,4 +99,28 @@ func main() {
 	if err := w.Write([]*Writer.LogRecord{record}); err != nil {
 		log.Fatalf("❌ שגיאה בכתיבה ל-BQ: %v", err)
 	}
+
+	    // 1. קונפיגורציית ברירת מחדל (מתחברת ל-localhost:8500)
+    config := api.DefaultConfig()
+
+    // 2. יצירת לקוח Consul
+    client, err := api.NewClient(config)
+    if err != nil {
+        panic(err)
+    }
+
+    // 3. קריאה ל-Key/Value
+    kv := client.KV()
+    pair, _, err := kv.Get("loadtool/config/speed_factor", nil)
+    if err != nil {
+        panic(err)
+    }
+
+    // 4. בדיקה אם הערך קיים
+    if pair == nil {
+        fmt.Println("Dont found value in consul")
+    } else {
+        fmt.Printf("The value from consul: %s\n", string(pair.Value))
+    }
+
 }

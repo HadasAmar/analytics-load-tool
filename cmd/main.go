3f6f835456478057536a4052c480bee54e19f196
Hadas <h0527622740@gmail.com>
2025-06-29 15:41:26 +0300

    fix: Arrange config in diffrent  classrooms

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"fmt"
+	"log"
+
 	"github.com/HadasAmar/analytics-load-tool/Model"
-	"github.com/HadasAmar/analytics-load-tool/Output"
+
+	// "github.com/HadasAmar/analytics-load-tool/Output"
 	"github.com/HadasAmar/analytics-load-tool/Parser"
 	"github.com/HadasAmar/analytics-load-tool/Reader"
-	"github.com/HadasAmar/analytics-load-tool/Simulator"
-	"github.com/HadasAmar/analytics-load-tool/Writer"
-	"log"
+
+	// "github.com/HadasAmar/analytics-load-tool/Simulator"
+	// "github.com/HadasAmar/analytics-load-tool/Writer"
+	// "log"
 	"os"
 	"path/filepath"
-	"time"
-	"github.com/hashicorp/consul/api"
+
+	// "time"
+	"github.com/HadasAmar/analytics-load-tool/configuration"
 )
 
 func main() {
@@ -49,79 +54,69 @@ func main() {
 	}
 
 	// שלב סימולציה
-	sim := Simulator.New(10.0)
+	// sim := Simulator.New(10.0)
 
-	// הגדרת יעד
-	ctx := context.Background()
-	w, err := Writer.NewBQWriter(ctx,
-		"credentials.json",
-		"platform-hackaton-2025",
-		"My_Try",
-		"loadtool_logs",
-	)
+	// // הגדרת יעד
+	// ctx := context.Background()
+	// w, err := Writer.NewBQWriter(ctx,
+	// 	"credentials.json",
+	// 	"platform-hackaton-2025",
+	// 	"My_Try",
+	// 	"loadtool_logs",
+	// )
+	// if err != nil {
+	// 	log.Fatalf("❌ שגיאה בהגדרת יעד BQ: %v", err)
+	// }
+
+	// // דוגמת כתיבה ל-BQ: רק רשומה אחת קבועה
+	// record := &Writer.LogRecord{
+	// 	CampaignID:          "abc123",
+	// 	AppID:               "com.kuku",
+	// 	Partner:             "partnerA",
+	// 	MediaSource:         "ms",
+	// 	UnmaskedMediaSource: "ms",
+	// 	AttributionType:     "install",
+	// 	Campaign:            "camp_test",
+	// 	Source:              "sourceA",
+	// 	AdID:                "ad1",
+	// 	AdsetID:             "adset1",
+	// 	AdsetName:           "set name",
+	// 	SiteID:              "site1",
+	// 	Ad:                  "adtext",
+	// 	LtvCountry:          "US",
+	// 	Installs:            15,
+	// 	Impressions:         100,
+	// 	Clicks:              30,
+	// 	Loyals:              3,
+	// 	OrganicInstalls:     1,
+	// 	OrganicImpressions:  5,
+	// 	OrganicClicks:       2,
+	// 	OrganicLoyals:       1,
+	// 	LogTime:             time.Now(),
+	// }
+
+	// // כתיבה לפלט jsonl
+	// for e := range sim.Stream(entries) {
+	// 	Output.WriteJSONL("output.jsonl", e)
+	// }
+
+	// // שליחה ל-BQ (רק הרשומה הבודדת)
+	// if err := w.Write([]*Writer.LogRecord{record}); err != nil {
+	// 	log.Fatalf("❌ שגיאה בכתיבה ל-BQ: %v", err)
+	// }
+
+	//configuration
+	// Create the client
+	client, err := configuration.NewConsulClient("localhost:8500")
 	if err != nil {
-		log.Fatalf("❌ שגיאה בהגדרת יעד BQ: %v", err)
+		log.Fatalf("Error creating Consul client: %v", err)
 	}
 
-	// דוגמת כתיבה ל-BQ: רק רשומה אחת קבועה
-	record := &Writer.LogRecord{
-		CampaignID:          "abc123",
-		AppID:               "com.kuku",
-		Partner:             "partnerA",
-		MediaSource:         "ms",
-		UnmaskedMediaSource: "ms",
-		AttributionType:     "install",
-		Campaign:            "camp_test",
-		Source:              "sourceA",
-		AdID:                "ad1",
-		AdsetID:             "adset1",
-		AdsetName:           "set name",
-		SiteID:              "site1",
-		Ad:                  "adtext",
-		LtvCountry:          "US",
-		Installs:            15,
-		Impressions:         100,
-		Clicks:              30,
-		Loyals:              3,
-		OrganicInstalls:     1,
-		OrganicImpressions:  5,
-		OrganicClicks:       2,
-		OrganicLoyals:       1,
-		LogTime:             time.Now(),
+	// Calling get_values
+	speedFactor, err := configuration.GetSpeedFactor(client)
+	if err != nil {
+		fmt.Println("Did not find value in Consul")
+	} else {
+		fmt.Printf("The value from Consul: %s\n", speedFactor)
 	}
-
-	// כתיבה לפלט jsonl
-	for e := range sim.Stream(entries) {
-		Output.WriteJSONL("output.jsonl", e)
-	}
-
-	// שליחה ל-BQ (רק הרשומה הבודדת)
-	if err := w.Write([]*Writer.LogRecord{record}); err != nil {
-		log.Fatalf("❌ שגיאה בכתיבה ל-BQ: %v", err)
-	}
-
-	//config..........
-	    // 1. קונפיגורציית ברירת מחדל (מתחברת ל-localhost:8500)
-    config := api.DefaultConfig()
-
-    // 2. יצירת לקוח Consul
-    client, err := api.NewClient(config)
-    if err != nil {
-        panic(err)
-    }
-
-    // 3. קריאה ל-Key/Value
-    kv := client.KV()
-    pair, _, err := kv.Get("loadtool/config/speed_factor", nil)
-    if err != nil {
-        panic(err)
-    }
-
-    // 4. בדיקה אם הערך קיים
-    if pair == nil {
-        fmt.Println("Dont found value in consul")
-    } else {
-        fmt.Printf("The value from consul: %s\n", string(pair.Value))
-    }
-
 }
